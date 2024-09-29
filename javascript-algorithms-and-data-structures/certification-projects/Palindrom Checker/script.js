af7486f8096f32747a69cfdf107e6e3ef957dc59
SophMe <116891325+SophMe@users.noreply.github.com>
2024-09-29 13:55:21 +0200

    add palindrome check logic and input validation

--- javascript-algorithms-and-data-structures/certification-projects/Palindrom Checker/script.js
+++ javascript-algorithms-and-data-structures/certification-projects/Palindrom Checker/script.js
@@ -1,7 +1,22 @@
 function isPalindromChecker(e) {
   e.preventDefault();
   const userInput = document.getElementById('text-input').value;
-  console.log(userInput);
-}
+  
+  if (!userInput) {
+    alert("Please input a value");
+    return;
+  }
+
+  const cleanedInput = cleanInputString(userInput);
+  const reversedInput = cleanedInput.split('').reverse().join('');
+  const isPalindrome = cleanedInput === reversedInput;
+
+
+  console.log("Input Value:", userInput);
+};
+
+function cleanInputString(str) {
+  return str.toLowerCase().replace(/[^a-z]/g, '');
+};
 
 document.getElementById('check-btn').addEventListener('click', isPalindromChecker);
