117b035985135c2a1bf2296bdf8531d2f4a7169d
SophMe <116891325+SophMe@users.noreply.github.com>
2024-10-06 13:06:33 +0200

    set up showAnimation function,  clean up code

--- javascript-algorithms-and-data-structures/skill-building-projects/Decimal to Binary Converter/script.js
+++ javascript-algorithms-and-data-structures/skill-building-projects/Decimal to Binary Converter/script.js
@@ -4,30 +4,39 @@ const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 
 // Step 2: Create function to display changes of user action (based on EventListener)
-function checkUserInput() {
-  // console.log(numberInput.value);
-  if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0) {
+const checkUserInput = () => {
+  const inputInt = parseInt(numberInput.value);
+  if (!numberInput.value || isNaN(inputInt) || inputInt < 0) {
     alert("Please provide a decimal number greater than or equal to 0");
     return;
   }
-  result.textContent = decimalToBinary(parseInt(numberInput.value));
+
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+  result.textContent = decimalToBinary(inputInt);
   numberInput.value = '';
 };
 
 // Step 3: Add EventListener to User action
 convertBtn.addEventListener('click', checkUserInput);
 numberInput.addEventListener('keydown', (e) => {
-  // console.log(e);
   if (e.key === "Enter") {
-    checkUserInput()
+    checkUserInput();
   }
 });
 
 // Step 4: Create function for the conversion
-function decimalToBinary (input) {
+const decimalToBinary = (input) => {
   if (input === 0 || input === 1) {
-    return String(input)
+    return String(input);
   } else {
     return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-}
+};
+
+// Step 5: Create function for animation
+const showAnimation = () => {
+
+};
