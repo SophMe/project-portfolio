dcf6f8d12c661f1cf1063117c0a4c89df58ef8bc
SophMe <116891325+SophMe@users.noreply.github.com>
2024-10-06 12:48:56 +0200

    refactor decimalToBinary

--- javascript-algorithms-and-data-structures/skill-building-projects/Decimal to Binary Converter/script.js
+++ javascript-algorithms-and-data-structures/skill-building-projects/Decimal to Binary Converter/script.js
@@ -10,7 +10,7 @@ function checkUserInput() {
     alert("Please provide a decimal number greater than or equal to 0");
     return;
   }
-  decimalToBinary(parseInt(numberInput.value));
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = '';
 };
 
@@ -25,14 +25,9 @@ numberInput.addEventListener('keydown', (e) => {
 
 // Step 4: Create function for the conversion
 function decimalToBinary (input) {
-  let binary = "";
-  if (input === 0) {
-    binary = "0";
+  if (input === 0 || input === 1) {
+    return String(input)
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-  result.innerText = binary;
-  while ( input > 0 ) {
-    binary = ( input % 2 )+ binary;
-    input = Math.floor(input / 2);
-  }
-  console.log(binary);
 }
