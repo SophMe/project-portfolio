44cb46276cc5d79343f739da154b3851efa2ea8b
SophMe <116891325+SophMe@users.noreply.github.com>
2024-10-10 15:54:43 +0200

    implement input validation for Roman numeral conversion

--- javascript-algorithms-and-data-structures/certification-projects/Roman Numeral Converter/script.js
+++ javascript-algorithms-and-data-structures/certification-projects/Roman Numeral Converter/script.js
@@ -5,13 +5,28 @@ const letterOutput = document.getElementById('output');
 
 // Check input
 const checkInput = () => {
-  console.log(numberInput.value);
-}
+  // console.log(numberInput.value);
+  const userInput = numberInput.value;
+  if (!numberInput.value) {
+    letterOutput.textContent = 'Please enter a valid number';
+    return;
+  } 
+  const num = parseInt(userInput);
+  if (num < 1) {
+    letterOutput.textContent = 'Please enter a number greater than or equal to 1'
+    return;
+  }
+  if (num >= 4000) {
+    letterOutput.textContent = 'Please enter a number less than or equal to 3999'
+    return;
+  }
+  letterOutput.textContent = arabicToRoman(num);
+};
 
 // EventListener for user action
 convertButton.addEventListener('click', checkInput);
 numberInput.addEventListener('keydown', (e) => {
-  console.log(e);
+  // console.log(e);
   if (e.key === 'Enter') {
     checkInput();
   }
