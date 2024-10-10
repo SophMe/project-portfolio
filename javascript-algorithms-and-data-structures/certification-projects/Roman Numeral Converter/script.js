ca80a5109886698b734aff6978a72ee209f96e3a
SophMe <116891325+SophMe@users.noreply.github.com>
2024-10-10 15:57:24 +0200

    implement function to convert Arabic numbers to Roman numerals

--- javascript-algorithms-and-data-structures/certification-projects/Roman Numeral Converter/script.js
+++ javascript-algorithms-and-data-structures/certification-projects/Roman Numeral Converter/script.js
@@ -3,6 +3,35 @@ const numberInput = document.getElementById('number');
 const convertButton = document.getElementById('convert-btn');
 const letterOutput = document.getElementById('output');
 
+// Convert numbers to roman letters
+const arabicToRoman = (num) => {
+  const romanNumerals = [
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' },
+  ];
+
+  let output = '';
+  for (const { value, numeral } of romanNumerals) {
+    while (num >= value) {
+      output += numeral;
+      num -= value;
+    }
+  };
+  // console.log(output);
+  return output;
+};
+
 // Check input
 const checkInput = () => {
   // console.log(numberInput.value);
