f1d82e60b134ab6bcdf81e805d8649abecf46bfb
SophMe <116891325+SophMe@users.noreply.github.com>
2024-10-11 17:53:47 +0200

    create updateUI function to display sorted numbers in the output area

--- javascript-algorithms-and-data-structures/skill-building-projects/Number Sorter/sript.js
+++ javascript-algorithms-and-data-structures/skill-building-projects/Number Sorter/sript.js
@@ -1,9 +1,20 @@
 const sortButton = document.getElementById('sort');
 
-  const sortInputArray = (event) => {
-    event.preventDefault();
-    const inputValues = [...document.getElementsByClassName('values-dropdown')]
-      .map(dropdown => Number(dropdown.value));
-  };
+const sortInputArray = (event) => {
+  event.preventDefault();
+
+  const inputValues = [
+    ...document.getElementsByClassName('values-dropdown')
+  ].map((dropdown) => Number(dropdown.value));
+  console.log('Input Values:', inputValues);
+  updateUI(inputValues);
+};
 
 sortButton.addEventListener('click', sortInputArray);
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  });
+};
