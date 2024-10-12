b69993e23610d6846c17d0118f3168f1a2f9f3c1
SophMe <116891325+SophMe@users.noreply.github.com>
2024-10-12 15:56:03 +0200

    implement bubble sort algorithm with swapping logic using temp variable

--- javascript-algorithms-and-data-structures/skill-building-projects/Number Sorter/sript.js
+++ javascript-algorithms-and-data-structures/skill-building-projects/Number Sorter/sript.js
@@ -11,8 +11,9 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName('values-dropdown')
   ].map((dropdown) => Number(dropdown.value));
-  console.log('Input Values:', inputValues);
-  updateUI(inputValues);
+  // console.log('Input Values:', inputValues);
+  const sortedValues = bubbleSort(inputValues);
+  updateUI(sortedValues);
 };
 
 // Helper function
@@ -21,4 +22,18 @@ const updateUI = (array = []) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
     outputValueNode.innerText = num;
   });
+};
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      // console.log(array, array[j], array[j+1]);
+      if (array[j] > array[j+1]) {
+        const temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+      }
+    }
+  }
+  return array;
 };
