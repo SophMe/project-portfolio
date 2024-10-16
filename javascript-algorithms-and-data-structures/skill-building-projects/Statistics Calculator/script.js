76b78c9d8161c5989a81cd555cf11ccc8af6224c
SophMe <116891325+SophMe@users.noreply.github.com>
2024-10-16 13:11:43 +0200

    display calculated mean in Statistics Calculator

--- javascript-algorithms-and-data-structures/skill-building-projects/Statistics Calculator/script.js
+++ javascript-algorithms-and-data-structures/skill-building-projects/Statistics Calculator/script.js
@@ -2,7 +2,9 @@ const getMean = (array) =>
   array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const calculate = () => {
-  const value = document.querySelector("#numbers").value;
+  const value = document.querySelector('#numbers').value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(element => Number(element)).filter(element => !isNaN(element));
+  const mean = getMean(numbers);
+  document.querySelector('#mean').textContent = mean;
 }
