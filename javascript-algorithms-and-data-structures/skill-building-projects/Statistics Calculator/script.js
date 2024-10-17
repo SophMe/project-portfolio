b7a8ffaee4dac383e08e5483128ab2ce135172ac
SophMe <116891325+SophMe@users.noreply.github.com>
2024-10-17 18:01:06 +0200

    create getMode function and display result

--- javascript-algorithms-and-data-structures/skill-building-projects/Statistics Calculator/script.js
+++ javascript-algorithms-and-data-structures/skill-building-projects/Statistics Calculator/script.js
@@ -13,6 +13,24 @@ const getMedian = (array) => {
   }
 };
 
+const getMode = (array) => {
+  const counts = {};
+  // array.forEach(el => counts[el] = (counts[el] || 0) + 1)
+  array.forEach((el) => {
+    if (counts[el]) {
+      counts[el] += 1;
+    } else {
+      counts[el] = 1;
+    }
+  })
+  if (new Set(Object.values(counts)).size === 1) {
+    return null
+  }
+  const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0];
+  const mode = Object.keys(counts).filter((el) => counts[el] === counts[highest]);
+  return mode.join(', ');
+};
+
 const calculate = () => {
   const value = document.querySelector('#numbers').value;
   const array = value.split(/,\s*/g);
@@ -20,7 +38,9 @@ const calculate = () => {
   
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  const mode = getMode(numbers);
 
   document.querySelector('#mean').textContent = mean;
   document.querySelector('#median').textContent = median;
+  document.querySelector('#mode').textContent = mode;
 };
