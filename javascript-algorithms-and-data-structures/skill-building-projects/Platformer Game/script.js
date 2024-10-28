ecda6f8dd50cbcc9f109084f2f456f5899939160
SophMe <116891325+SophMe@users.noreply.github.com>
2024-10-28 10:27:44 +0100

    fix animate function

--- javascript-algorithms-and-data-structures/skill-building-projects/Platformer Game/script.js
+++ javascript-algorithms-and-data-structures/skill-building-projects/Platformer Game/script.js
@@ -159,32 +159,32 @@ const animate = () => {
       platform.position.x += 5;
     });
   }
-};
 
-// Collision Detection
-platforms.forEach(platform => {
-  const collisionDetectionRules = [
-    player.position.y + player.height <= platform.position.y,
-    player.position.y + player.height + player.velocity.y >= platform.position.y,
-    player.position.x >= platform.position.x - player.width / 2,
-    player.position.x <= platform.position.x + platform.width - player.width / 3
-  ];
-  // rule => rule is used to confirm that each condition in the collisionDetectionRules array evaluates to true
-  if (collisionDetectionRules.every(rule => rule)) {
-    player.velocity.y = 0;
-    return;
-  }
-  const platformDetectionRules = [
-    player.position.x >= platform.position.x - player.width / 2,
-    player.position.x <= platform.position.x + platform.width - player.width / 3,
-    player.position.y + player.height >= platform.position.y,
-    player.position.y <= platform.position.y + platform.height,
-  ];
-  if (platformDetectionRules.every(rule => rule)) {
-    player.position.y = platform.position.y + player.height;
-    player.velocity.y = gravity;
-  }
-});
+  // Collision Detection
+  platforms.forEach(platform => {
+    const collisionDetectionRules = [
+      player.position.y + player.height <= platform.position.y,
+      player.position.y + player.height + player.velocity.y >= platform.position.y,
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <= platform.position.x + platform.width - player.width / 3
+    ];
+    // rule => rule is used to confirm that each condition in the collisionDetectionRules array evaluates to true
+    if (collisionDetectionRules.every(rule => rule)) {
+      player.velocity.y = 0;
+      return;
+    }
+    const platformDetectionRules = [
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <= platform.position.x + platform.width - player.width / 3,
+      player.position.y + player.height >= platform.position.y,
+      player.position.y <= platform.position.y + platform.height,
+    ];
+    if (platformDetectionRules.every(rule => rule)) {
+      player.position.y = platform.position.y - player.height;
+      player.velocity.y = 0;
+    }
+  });
+};
 
 // Key Controls
 const keys = {
