dd0e11ba634113bbaa412b01786227ecfa882caa
SophMe <116891325+SophMe@users.noreply.github.com>
2024-11-13 17:20:52 +0100

    refactoring from fCC

--- javascript-algorithms-and-data-structures/skill-building-projects/Dice Game/script.js
+++ javascript-algorithms-and-data-structures/skill-building-projects/Dice Game/script.js
@@ -16,8 +16,6 @@ let score = 0;
 let round = 1; 
 let rolls = 0;
 
-const maxRolls = 3;
-
 const rollDice = () => {
   diceValuesArr = [];
   for (let i = 0; i < 5; i++) {
@@ -30,13 +28,13 @@ const rollDice = () => {
 };
 
 rollDiceBtn.addEventListener("click", () => {
-  if (rolls >= maxRolls) {
-    alert("You must select a score");
+  if (rolls === 3) {
+    alert("You have made three rolls this round. Please select a score.");
   } else {
-    rollDice();
     rolls++;
-    rollsElement.textContent = `Rolls: ${rolls}`;
-}
+    rollDice();
+    
+  }
 });
 
 rulesBtn.addEventListener("click", () => {
