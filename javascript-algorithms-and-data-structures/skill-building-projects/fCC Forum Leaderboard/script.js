a88caa7a976829d2c21ea3df6300dec0f85e036d
SophMe <116891325+SophMe@users.noreply.github.com>
2024-12-03 18:39:22 +0100

    add showLatestPosts function with destructered topic_list and users, and integrate with fetchData

--- javascript-algorithms-and-data-structures/skill-building-projects/fCC Forum Leaderboard/script.js
+++ javascript-algorithms-and-data-structures/skill-building-projects/fCC Forum Leaderboard/script.js
@@ -9,10 +9,16 @@ const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
     const data = await res.json();
-    console.log(data);
+    showLatestPosts(data);
   } catch (err) {
     console.log(err);
   }
 };
 
 fetchData();
+
+const showLatestPosts = (data) => {
+  const { topic_list, users } = data;
+  const { topics } = topic_list;
+  
+};
