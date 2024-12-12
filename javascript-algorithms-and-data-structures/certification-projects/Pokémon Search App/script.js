5594eee3612430c840ea2d3b353a6fa34af3f65a
SophMe <116891325+SophMe@users.noreply.github.com>
2024-12-12 15:48:35 +0100

    access DOM elements for Pokémon search and stats display

---
+++ javascript-algorithms-and-data-structures/certification-projects/Pokémon Search App/script.js
@@ -0,0 +1,16 @@
+// Access DOM Elements
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+
+const pokemonNameElement = document.getElementById('pokemon-name');
+const pokemonIdElement = document.getElementById('pokemon-id');
+const weightElement = document.getElementById('weight');
+const heightElement = document.getElementById('height');
+const typesElement = document.getElementById('types');
+
+const hpElement = document.getElementById('hp');
+const attackElement = document.getElementById('attack');
+const defenseElement = document.getElementById('defense');
+const specialAttackElement = document.getElementById('special-attack');
+const specialDefenseElement = document.getElementById('special-defense');
+const speedElement = document.getElementById('speed');
